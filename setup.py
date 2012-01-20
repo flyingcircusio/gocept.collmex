3433aabaa222c5f17ac462ddd6f90cfad0771e90
mac <mac@14c79854-3af7-0310-aa0e-852fec42f1e4>
2012-01-20 17:26:00 +0000

    Back to development: 0.10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.collmex',
-    version='0.9',
+    version='0.10dev',
     author='gocept',
     author_email='mail@gocept.com',
     description='Python-bindings for the Collmex import/export API',
