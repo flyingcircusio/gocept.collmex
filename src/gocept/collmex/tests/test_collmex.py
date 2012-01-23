058ea661d2a3ed917eb685c3298625efce24bee7
Michael Howitz <mh@gocept.com>
2012-01-23 12:11:13 +0000

    Made testing helper ``collmex_login()`` a method: ``gocept.collmex.collmex.Collmex.browser_login()``

--- src/gocept/collmex/tests/test_collmex.py
+++ src/gocept/collmex/tests/test_collmex.py
@@ -16,3 +16,10 @@ class TestCollmex(unittest.TestCase):
             collmex_invalid.get_invoices(customer_id='10000')
         self.assertEqual("('101004', 'Benutzer oder Kennwort nicht korrekt')",
                          str(err.exception))
+
+    def test_browser_login_authenticates_user_using_browser(self):
+        import gocept.collmex.testing
+        collmex = gocept.collmex.testing.get_collmex()
+        browser = collmex.browser_login()
+        self.assertTrue(
+            browser.getLink('Projekt-Verbrauch').url.endswith(',vbrp'))
