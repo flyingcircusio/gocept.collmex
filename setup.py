9dc0a8d2c6d4d347ad13dffc9ad965ebe8cf5d9c
Christian Theune <ct@gocept.com>
2012-02-17 14:35:35 +0100

    Preparing release 1.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.collmex',
-    version='1.1.1dev',
+    version='1.1.1',
     author='gocept',
     author_email='mail@gocept.com',
     description='Python-bindings for the Collmex import/export API',
