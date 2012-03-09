a3d90374e8cd4091c4007256fc7a5c7442a1fd94
Christian Zagrodnick <cz@flyingcircus.io>
2012-03-09 14:10:28 +0100

    Back to development: 1.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.collmex',
-    version='1.2.1',
+    version='1.2.2.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     description='Python-bindings for the Collmex import/export API',
