191fc13595cffb79148e9092f5d6b87885a8f912
Florian Pilz <fp@gocept.com>
2013-09-13 08:13:44 +0200

    adjust dockets runner to pick correct file when using bin/test - fox will use a search pattern, which already picks the right file

--- src/gocept/collmex/tests/test_doctest.py
+++ src/gocept/collmex/tests/test_doctest.py
@@ -2,11 +2,15 @@
 # See also LICENSE.txt
 
 import doctest
+import six
 
 
 def test_suite():
+    file = 'py27_doctest.txt'
+    if six.PY3:
+        file = 'py32_doctest.txt'
     return doctest.DocFileSuite(
-        'README.txt',
+        file,
         package='gocept.collmex',
         optionflags=(doctest.NORMALIZE_WHITESPACE |
                      doctest.ELLIPSIS))
