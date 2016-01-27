40df20769a6a8bd1cd36cbebeba4bdd1a8818de7
Florian Pilz <fp@gocept.com>
2016-01-27 09:22:03 +0100

    Preparing release 1.8.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.collmex',
-    version='1.7.1.dev0',
+    version='1.8.0',
     author='gocept',
     author_email='mail@gocept.com',
     description='Python-bindings for the Collmex import/export API',
