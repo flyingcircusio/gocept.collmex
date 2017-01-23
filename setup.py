4da917cedd882445b99ce0abd1f47f6ff72b1d52
Daniel Havlik <dh@gocept.com>
2017-01-23 21:38:16 +0100

    Back to development: 1.8.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.collmex',
-    version='1.8.2',
+    version='1.8.3.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     description='Python-bindings for the Collmex import/export API',
