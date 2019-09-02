9d03be11fbbdc5319562f8c47af592da1ea93547
Michael Howitz <mh@gocept.com>
2019-09-02 10:20:16 +0200

    Fix test coverage: Never ignore any code!

--- src/gocept/__init__.py
+++ src/gocept/__init__.py
@@ -1,4 +1,1 @@
-# Copyright (c) 2008 gocept gmbh & co. kg
-# See also LICENSE.txt
-# namespace package boilerplate
-__import__('pkg_resources').declare_namespace(__name__)
+__import__('pkg_resources').declare_namespace(__name__)  # pragma: no cover
