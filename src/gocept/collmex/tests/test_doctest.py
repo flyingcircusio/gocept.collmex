0bf48ae44b8ef47b7161d874822691ae2f26f24e
Michael Howitz <mh@gocept.com>
2023-10-12 08:24:56 +0200

    Fix PyPy3 job in GHA. (#30)

---
+++ src/gocept/collmex/tests/test_doctest.py
@@ -0,0 +1,13 @@
+from zope.testing import renormalizing
+import doctest
+import re
+
+
+checker = renormalizing.RENormalizing([
+    # Ignore output returned by GHA for PyPy3
+    (re.compile('/etc/ssl/certs/ca-certificates.crt /etc/ssl/certs'), ''),
+])
+
+
+def test_suite():
+    return doctest.DocFileSuite('../doctest.rst', checker=checker)
